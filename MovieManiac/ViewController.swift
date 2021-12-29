6169a2d79fc100df0aced630028d8e1f292682aa
bakaiismailov <ismailov.time@gmail.com>
2021-12-29 14:21:37 +0600

    Initial Commit

---
+++ MovieManiac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieManiac
+//
+//  Created by Bakai Ismailov on 29/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
